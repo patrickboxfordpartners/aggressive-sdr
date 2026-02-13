3e4ca5e71d0ad7b510ba3a67b90083da757883aa
patrickboxfordpartners <patrick@boxfordpartners.com>
2026-02-13 15:19:47 -0800

    Add src/inngest/index.ts

---
+++ src/inngest/index.ts
@@ -0,0 +1,4 @@
+export { inngest as client } from "./client";
+export { exampleCron } from "./functions/example-cron";
+export { executeAutomation } from "./functions/execute-automation";
+export { generateScheduledReports } from "./functions/generate-scheduled-reports";
