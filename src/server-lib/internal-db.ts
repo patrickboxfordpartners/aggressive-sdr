55d4805a7bd3261c4c6714f58ed70a449a370450
patrickboxfordpartners <patrick@boxfordpartners.com>
2026-02-13 15:00:26 -0800

    Add src/server-lib/internal-db.ts

---
+++ src/server-lib/internal-db.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { vybeRequestInterceptor } from "./vybe-request-interceptor";
+
+const vybeDomain = process.env.NEXT_PUBLIC_VYBE_INTEGRATIONS_DOMAIN ?? "https://vybe.build";
+
+export const internalDbClient = axios.create({
+  baseURL: vybeDomain + "/api/database",
+  withCredentials: true,
+});
+
+internalDbClient.interceptors.request.use(vybeRequestInterceptor);
