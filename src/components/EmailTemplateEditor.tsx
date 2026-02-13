280cb378d5568969af85cdcc5b1b0ad6207917e6
patrickboxfordpartners <patrick@boxfordpartners.com>
2026-02-13 15:34:59 -0800

    Add EmailTemplateEditor.tsx (stub)

---
+++ src/components/EmailTemplateEditor.tsx
@@ -0,0 +1,3 @@
+// See FULL_SOURCE_DUMP.md - Email template editor components
+// VariableInserter, UnknownVarWarning, EmailSubjectField, EmailBodyField, TemplatePicker, SaveTemplateDialog
+export {};
